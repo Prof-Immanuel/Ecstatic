c7b78349290091dfff047e1305e477c609e1fefb
Prof-Immanuel <prof.immanuel2003@gmail.com>
2025-07-25 11:16:03 +0200

    Fixing pillow config

--- home/urls.py
+++ home/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     path('register/', views.signup, name="register"),
     path('login/', views.user_login, name='login'),
     path('logout', views.user_logout, name='logout'),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
